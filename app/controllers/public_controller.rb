241a9b458567e01d6c2043fa8b9e21c2bc4b0658
Dillon Hafer <example@email.com>
2011-07-01 19:55:02 -0500

    added birthday app.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -25,4 +25,18 @@ class PublicController < ApplicationController
   
   end
 
+  def birthday
+    @birthday = Date.new(2011, 01, 01)
+  end
+  
+  def update_birthday
+    @birthday = Date.new params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i
+    if @birthday == Date.today
+      flash[:good_birthday] = "Today is your birthday!"
+    else
+      flash[:bad_birthday] = "Today is not your birthday."
+    end
+      redirect_to :action => "birthday"
+  end
+
 end
